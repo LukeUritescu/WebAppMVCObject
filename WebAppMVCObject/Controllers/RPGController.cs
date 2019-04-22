3d71bcac1ce841db26f2e86a0c282c53d8756c04
Luke Uritescu <Luritescu@gmail.com>
2019-04-22 13:24:52 -0500

    Add project files.

---
+++ WebAppMVCObject/Controllers/RPGController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAppMVCObject.Models;
+
+namespace WebAppMVCObject.Controllers
+{
+    public class RPGController : Controller
+    {
+        List<IRPG> rpg;
+
+        public RPGController()
+        {
+            rpg = new List<IRPG>()
+            {
+                new RPG()
+            };
+        }
+
+        public ActionResult Index()
+        {
+            return View(rpg);
+        }
+
+        public ActionResult Details(string Name)
+        {
+            return View(rpg.Where(n => n.Name == Name).FirstOrDefault());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+
+            return View(rpg[id]);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAppMVCObject/Models/IRPG.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMVCObject.Models
+{
+    public interface IRPG : IAboutable
+    {
+         string Name { get;  set; }
+         int Strength { get;  set; }
+         int Dexterity { get; set; }
+         int Constitution { get; set; }
+         int Intelligence { get; set; }
+         int Wisdom { get; set; }
+         int Charisma { get; set; }
+
+
+
+    }
+
+    public interface IAboutable
+    {
+        string About();
+    }
+}
